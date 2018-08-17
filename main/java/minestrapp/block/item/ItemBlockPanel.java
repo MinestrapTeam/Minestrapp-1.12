e1c7af59386590a182fba9c9286d9a0283209033
Delocuro <delocuro@gmail.com>
2018-08-16 23:29:28 -0400

    Panelhandeling
    - Fixed all placement issues with Panels.
    - Panels can now connect to adjacent perpendicular Panels to form
    corners! Hitboxes are a little wonky still, but the collisions and
    models appear to be working perfect.

--- main/java/minestrapp/block/item/ItemBlockPanel.java
+++ main/java/minestrapp/block/item/ItemBlockPanel.java
@@ -3,7 +3,9 @@ package minestrapp.block.item;
 import minestrapp.block.util.BlockPanelBase;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockSlab;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
@@ -16,6 +18,8 @@ import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemBlockPanel extends MItemBlock
 {
@@ -26,58 +30,85 @@ public class ItemBlockPanel extends MItemBlock
 	
 	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
     {
+		//Get the player's held item.
         ItemStack itemstack = player.getHeldItem(hand);
-
+        
+        //If the player is holding an item and can edit the block in question,
         if (!itemstack.isEmpty() && player.canPlayerEdit(pos.offset(facing), facing, itemstack))
         {
+        	//save the blockstate at the position where the player is trying to place the panel.
         	IBlockState state = worldIn.getBlockState(pos);
         	
+        	//System.out.println(state.getBlock().getUnlocalizedName());
+        	
+        	//If the block is a matching panel and it isn't already a double-panel,
         	if (state.getBlock() == this.block && state.getValue(BlockPanelBase.FACING).getAxis() != EnumFacing.Axis.Y)
             {
+        		//check its facing.
         		EnumFacing panelFacing = state.getValue(BlockPanelBase.FACING);
         		
+        		//If it is facing the opposite direction of the face the player clicked on,
         		if (facing == panelFacing.getOpposite())
         		{
+        			//and there are no entities in the way, and it successfully places the double-panel,
         			if (worldIn.checkNoEntityCollision(Block.FULL_BLOCK_AABB.offset(pos)) && worldIn.setBlockState(pos, state.withProperty(BlockPanelBase.FACING, EnumFacing.UP), 11))
                     {
+        				//play a sound and reduce the itemstack by 1.
         				SoundType soundtype = this.block.getSoundType(state.withProperty(BlockPanelBase.FACING, EnumFacing.UP), worldIn, pos, player);
                         worldIn.playSound(player, pos, soundtype.getPlaceSound(), SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
                         itemstack.shrink(1);
 
+                        //Then increase the player's Placed Block criteria value.
                         if (player instanceof EntityPlayerMP)
                         {
                             CriteriaTriggers.PLACED_BLOCK.trigger((EntityPlayerMP)player, pos, itemstack);
                         }
                     }
         			
+        			//Item successfully used.
         			return EnumActionResult.SUCCESS;
         		}
             }
         	
-        	BlockPos newpos = pos.offset(facing.getOpposite());
-        	state = worldIn.getBlockState(newpos);
+        	state = worldIn.getBlockState(pos.offset(facing));
         	
-        	if (state.getBlock() == this.block && state.getValue(BlockPanelBase.FACING) == this.getStateForPlacement(worldIn, pos, facing, hitX, hitY, hitZ, itemstack.getMetadata(), player).getOpposite())
-            {	
-        		if (worldIn.checkNoEntityCollision(Block.FULL_BLOCK_AABB.offset(newpos)) && worldIn.setBlockState(newpos, state.withProperty(BlockPanelBase.FACING, EnumFacing.UP), 11))
+        	if(state.getBlock() == this.block && state.getValue(BlockPanelBase.FACING).getAxis() != EnumFacing.Axis.Y)
+        	{
+        		if (worldIn.checkNoEntityCollision(Block.FULL_BLOCK_AABB.offset(pos.offset(facing))) && worldIn.setBlockState(pos.offset(facing), state.withProperty(BlockPanelBase.FACING, EnumFacing.UP), 11))
                 {
-    				SoundType soundtype = this.block.getSoundType(state.withProperty(BlockPanelBase.FACING, EnumFacing.UP), worldIn, newpos, player);
+    				//play a sound and reduce the itemstack by 1.
+    				SoundType soundtype = this.block.getSoundType(state.withProperty(BlockPanelBase.FACING, EnumFacing.UP), worldIn, pos, player);
                     worldIn.playSound(player, pos, soundtype.getPlaceSound(), SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
                     itemstack.shrink(1);
 
+                    //Then increase the player's Placed Block criteria value.
                     if (player instanceof EntityPlayerMP)
                     {
-                        CriteriaTriggers.PLACED_BLOCK.trigger((EntityPlayerMP)player, newpos, itemstack);
+                        CriteriaTriggers.PLACED_BLOCK.trigger((EntityPlayerMP)player, pos, itemstack);
                     }
                 }
     			
+    			//Item successfully used.
     			return EnumActionResult.SUCCESS;
-            }
+        	}
         	
         	return super.onItemUse(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
         }
         
         return EnumActionResult.FAIL;
+    }
+	
+	@SideOnly(Side.CLIENT)
+    public boolean canPlaceBlockOnSide(World worldIn, BlockPos pos, EnumFacing side, EntityPlayer player, ItemStack stack)
+    {
+        IBlockState iblockstate = worldIn.getBlockState(pos.offset(side));
+       
+        if (iblockstate.getBlock() == this.block && iblockstate.getValue(BlockPanelBase.FACING).getAxis() != EnumFacing.Axis.Y)
+        {
+            return true;
+        }
+
+        return super.canPlaceBlockOnSide(worldIn, pos, side, player, stack);
     }
 	
 	public static EnumFacing getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
