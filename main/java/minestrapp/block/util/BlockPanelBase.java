5ca3ef6c68896e23cb0e13d8c4745e371ddea2a0
Delocuro <delocuro@gmail.com>
2018-08-18 02:05:16 -0400

    100 Years Json Adventure
    - Fixed a ton of Panel functionality issues -- they should be 99%
    functional now. :D
    - Added Panels to the Minestrapp Wood tab so we don't have to keep
    summoning stuff in.
    - Added the rest of the Wood Panel variants. Tomorrow will be the
    long-haul of adding 500,000 stupid jsons for all the Stone variants...

--- main/java/minestrapp/block/util/BlockPanelBase.java
+++ main/java/minestrapp/block/util/BlockPanelBase.java
@@ -1,32 +1,42 @@
 package minestrapp.block.util;
 
 import java.util.List;
+import java.util.Random;
 
 import javax.annotation.Nullable;
 
 import com.google.common.base.Predicate;
+import com.google.common.collect.Lists;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockHorizontal;
-import net.minecraft.block.BlockSlab;
-import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
-import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockPanelBase extends BlockBase
 {
@@ -61,22 +71,29 @@ public class BlockPanelBase extends BlockBase
 	protected static final AxisAlignedBB AABB_NE = new AxisAlignedBB(0.5D, 0D, 0D, 1D, 1D, 0.5D);
 	protected static final AxisAlignedBB AABB_NW = new AxisAlignedBB(0D, 0D, 0D, 0.5D, 1D, 0.5D);
 	
-	private Block block;
-	private int meta;
+	private final Block block;
+	private final int meta;
 	
-	public BlockPanelBase(Block block)
+	public BlockPanelBase(Block block, CreativeTabs tab)
 	{
 		super("panel_" + block.getUnlocalizedName().substring(5), block.getMaterial(block.getDefaultState()), MapColor.AIR, block.getSoundType(), 0F, block.getHarvestTool(block.getDefaultState()), block.getHarvestLevel(block.getDefaultState()));
 		this.block = block;
 		this.meta = 0;
+		this.setLightOpacity(255);
+		this.useNeighborBrightness = true;
+		this.setCreativeTab(tab);
 		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH).withProperty(CONNECTED, Integer.valueOf(0)));
 	}
 	
-	public BlockPanelBase(Block block, int meta, String variant)
+	public BlockPanelBase(Block block, CreativeTabs tab, int meta, String variant)
 	{
 		super("panel_" + block.getUnlocalizedName().substring(5) + "_" + variant, block.getStateFromMeta(meta).getMaterial(), MapColor.AIR, block.getSoundType(), 0F, block.getHarvestTool(block.getStateFromMeta(meta)), block.getHarvestLevel(block.getStateFromMeta(meta)));
+		this.block = block;
 		this.meta = meta;
-		this.meta = meta;
+		this.setLightOpacity(255);
+		this.useNeighborBrightness = true;
+		this.setCreativeTab(tab);
+		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH).withProperty(CONNECTED, Integer.valueOf(0)));
 	}
 
 	public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn, boolean isActualState)
@@ -135,18 +152,105 @@ public class BlockPanelBase extends BlockBase
         }
     }
 	
+	private static List<AxisAlignedBB> getCollisionBoxList(IBlockState state)
+    {
+		List<AxisAlignedBB> list = Lists.<AxisAlignedBB>newArrayList();
+		EnumFacing facing = state.getValue(FACING);
+        int connected = Integer.valueOf(state.getValue(CONNECTED));
+        
+        if(facing == EnumFacing.UP)
+        	list.add(FULL_BLOCK_AABB);
+        else if(facing == EnumFacing.NORTH)
+        {
+        	if(connected != 2)
+				list.add(AABB_NW);
+        	if(connected != 1)
+        		list.add(AABB_NE);
+        	if(connected == 3)
+        		list.add(AABB_SW);
+        	if(connected == 4)
+        		list.add(AABB_SE);
+        }
+        else if(facing == EnumFacing.SOUTH)
+        {
+        	if(connected == 1)
+        		list.add(AABB_NW);
+        	if(connected == 2)
+        		list.add(AABB_NE);
+        	if(connected != 4)
+        		list.add(AABB_SW);
+        	if(connected != 3)
+        		list.add(AABB_SE);
+        }
+        else if(facing == EnumFacing.EAST)
+        {
+        	if(connected == 1)
+        		list.add(AABB_NW);
+        	if(connected != 4)
+        		list.add(AABB_NE);
+        	if(connected == 3)
+        		list.add(AABB_SW);
+        	if(connected != 2)
+        		list.add(AABB_SE);
+        }
+        else if(facing == EnumFacing.WEST)
+        {
+        	if(connected != 3)
+        		list.add(AABB_NW);
+        	if(connected == 2)
+        		list.add(AABB_NE);
+        	if(connected != 1)
+        		list.add(AABB_SW);
+        	if(connected == 4)
+        		list.add(AABB_SE);
+        }
+        
+        return list;
+    }
+	
 	public AxisAlignedBB getBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos)
     {
-        if(blockState.getValue(FACING) == EnumFacing.NORTH)
-        	return AABB_NORTH;
-        if(blockState.getValue(FACING) == EnumFacing.EAST)
-        	return AABB_EAST;
-        if(blockState.getValue(FACING) == EnumFacing.SOUTH)
-        	return AABB_SOUTH;
-        if(blockState.getValue(FACING) == EnumFacing.WEST)
-        	return AABB_WEST;
-        else
-        	return FULL_BLOCK_AABB;
+		IBlockState state = this.getActualState(blockState, worldIn, pos);
+		EnumFacing facing = blockState.getValue(FACING);
+		int connected = state.getValue(CONNECTED);
+		
+		if(facing == EnumFacing.NORTH)
+        {
+        	if(connected == 0)
+        		return AABB_NORTH;
+        	if(connected == 1)
+        		return AABB_NW;
+        	if(connected == 2)
+        		return AABB_NE;
+        }
+		if(facing == EnumFacing.SOUTH)
+        {
+        	if(connected == 0)
+        		return AABB_SOUTH;
+        	if(connected == 3)
+        		return AABB_SW;
+        	if(connected == 4)
+        		return AABB_SE;
+        }
+		if(facing == EnumFacing.EAST)
+        {
+        	if(connected == 0)
+        		return AABB_EAST;
+        	if(connected == 2)
+        		return AABB_NE;
+        	if(connected == 4)
+        		return AABB_SE;
+        }
+		if(facing == EnumFacing.WEST)
+        {
+        	if(connected == 0)
+        		return AABB_WEST;
+        	if(connected == 1)
+        		return AABB_NW;
+        	if(connected == 3)
+        		return AABB_SW;
+        }
+        return FULL_BLOCK_AABB;
     }
 	
 	public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos)
@@ -243,14 +347,22 @@ public class BlockPanelBase extends BlockBase
         return state.withProperty(CONNECTED, Integer.valueOf(quadrant));
     }
 	
+	public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face)
+    {
+		if(state.getValue(FACING) == EnumFacing.UP || state.getValue(FACING) == face)
+			return BlockFaceShape.SOLID;
+		else
+			return BlockFaceShape.UNDEFINED;
+    }
+	
 	public boolean isOpaqueCube(IBlockState state)
     {
-        return false;
+        return state.getValue(FACING) == EnumFacing.UP;
     }
 
     public boolean isFullCube(IBlockState state)
     {
-        return false;
+    	return state.getValue(FACING) == EnumFacing.UP;
     }
     
     public IBlockState getStateFromMeta(int meta)
@@ -289,12 +401,6 @@ public class BlockPanelBase extends BlockBase
 			}
 		}
     }
-	
-	public boolean canPlaceBlockAt(World worldIn, BlockPos pos)
-    {
-		return super.canPlaceBlockAt(worldIn, pos);
-        //return worldIn.getBlockState(pos).getBlock().isReplaceable(worldIn, pos) || worldIn.getBlockState(pos).getBlock() == this;
-    }
     
 	public static EnumFacing getFacing(int meta)
     {
@@ -315,9 +421,184 @@ public class BlockPanelBase extends BlockBase
     {
         return new BlockStateContainer(this, new IProperty[] {FACING, CONNECTED});
     }
-
-    public BlockFaceShape getBlockFaceShape(IBlockAccess world, IBlockState state, BlockPos pos, EnumFacing facing)
+    
+    @SideOnly(Side.CLIENT)
+    public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand)
     {
-        return BlockFaceShape.UNDEFINED;
+        this.block.randomDisplayTick(stateIn, worldIn, pos, rand);
     }
+
+    public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn)
+    {
+        this.block.onBlockClicked(worldIn, pos, playerIn);
+    }
+
+    /**
+     * Called after a player destroys this Block - the posiiton pos may no longer hold the state indicated.
+     */
+    public void onBlockDestroyedByPlayer(World worldIn, BlockPos pos, IBlockState state)
+    {
+        this.block.onBlockDestroyedByPlayer(worldIn, pos, state);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public int getPackedLightmapCoords(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        return this.block.getStateFromMeta(meta).getPackedLightmapCoords(source, pos);
+    }
+
+    @Deprecated
+    public float getBlockHardness(IBlockState blockState, World worldIn, BlockPos pos)
+    {
+        return this.block.getBlockHardness(blockState, worldIn, pos);
+    }
+    
+    /**
+     * Returns how much this block can resist explosions from the passed in entity.
+     */
+    public float getExplosionResistance(Entity exploder)
+    {
+        return this.block.getExplosionResistance(exploder);
+    }
+
+    /**
+     * How many world ticks before ticking
+     */
+    public int tickRate(World worldIn)
+    {
+        return this.block.tickRate(worldIn);
+    }
+
+    public Vec3d modifyAcceleration(World worldIn, BlockPos pos, Entity entityIn, Vec3d motion)
+    {
+        return this.block.modifyAcceleration(worldIn, pos, entityIn, motion);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer()
+    {
+        return this.block.getBlockLayer();
+    }
+
+    /**
+     * Returns if this block is collidable. Only used by fire, although stairs return that of the block that the stair
+     * is made of (though nobody's going to make fire stairs, right?)
+     */
+    public boolean isCollidable()
+    {
+        return this.block.isCollidable();
+    }
+
+    public boolean canCollideCheck(IBlockState state, boolean hitIfLiquid)
+    {
+        return this.block.canCollideCheck(state, hitIfLiquid);
+    }
+
+    /**
+     * Checks if this block can be placed exactly at the given position.
+     */
+    public boolean canPlaceBlockAt(World worldIn, BlockPos pos)
+    {
+        return this.block.canPlaceBlockAt(worldIn, pos);
+    }
+
+    /**
+     * Called after the block is set in the Chunk data, but before the Tile Entity is set
+     */
+    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state)
+    {
+        this.block.getStateFromMeta(meta).neighborChanged(worldIn, pos, Blocks.AIR, pos);
+        this.block.onBlockAdded(worldIn, pos, this.block.getStateFromMeta(meta));
+    }
+
+    /**
+     * Called serverside after this block is replaced with another in Chunk, but before the Tile Entity is updated
+     */
+    public void breakBlock(World worldIn, BlockPos pos, IBlockState state)
+    {
+        this.block.breakBlock(worldIn, pos, this.block.getStateFromMeta(meta));
+    }
+
+    /**
+     * Called when the given entity walks on this Block
+     */
+    public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn)
+    {
+        this.block.onEntityWalk(worldIn, pos, entityIn);
+    }
+
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
+    {
+        this.block.updateTick(worldIn, pos, state, rand);
+    }
+
+    /**
+     * Called when the block is right clicked by a player.
+     */
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        return this.block.onBlockActivated(worldIn, pos, this.block.getStateFromMeta(meta), playerIn, hand, EnumFacing.DOWN, 0.0F, 0.0F, 0.0F);
+    }
+
+    /**
+     * Called when this Block is destroyed by an Explosion
+     */
+    public void onBlockDestroyedByExplosion(World worldIn, BlockPos pos, Explosion explosionIn)
+    {
+        this.block.onBlockDestroyedByExplosion(worldIn, pos, explosionIn);
+    }
+
+    /**
+     * Get the MapColor for this Block and the given BlockState
+     */
+    public MapColor getMapColor(IBlockState state, IBlockAccess worldIn, BlockPos pos)
+    {
+        return this.block.getMapColor(this.block.getStateFromMeta(meta), worldIn, pos);
+    }
+    
+    @Nullable
+    public RayTraceResult collisionRayTrace(IBlockState blockState, World worldIn, BlockPos pos, Vec3d start, Vec3d end)
+    {
+        List<RayTraceResult> list = Lists.<RayTraceResult>newArrayList();
+
+        for (AxisAlignedBB axisalignedbb : getCollisionBoxList(this.getActualState(blockState, worldIn, pos)))
+        {
+            list.add(this.rayTrace(pos, start, end, axisalignedbb));
+        }
+
+        RayTraceResult raytraceresult1 = null;
+        double d1 = 0.0D;
+
+        for (RayTraceResult raytraceresult : list)
+        {
+            if (raytraceresult != null)
+            {
+                double d0 = raytraceresult.hitVec.squareDistanceTo(end);
+
+                if (d0 > d1)
+                {
+                    raytraceresult1 = raytraceresult;
+                    d1 = d0;
+                }
+            }
+        }
+
+        return raytraceresult1;
+    }
+    
+    public void dropBlockAsItemWithChance(World worldIn, BlockPos pos, IBlockState state, float chance, int fortune)
+    {
+        if (!worldIn.isRemote && !worldIn.restoringBlockSnapshots)
+        {
+        	if(state.getValue(FACING) == EnumFacing.UP)
+        		spawnAsEntity(worldIn, pos, new ItemStack(Item.getItemFromBlock(this), 2, 0));
+        	else
+        		spawnAsEntity(worldIn, pos, new ItemStack(Item.getItemFromBlock(this), 1, 0));
+        }
+    }
+    
+    public boolean canSilkHarvest()
+	{
+		return false;
+	}
 }
